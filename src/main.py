d0f904c09aafc1c5d69e78cd74147633d6d8c600
Maulik Shah <maulik.shah715@gmail.com>
2021-03-26 13:05:41 +0530

    Now the browser can open google.com and you cans earch on it

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 import sys
+from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("https://google.com"))
+        self.setCentralWidget(self.browser)
         self.showMaximized()
 
 
